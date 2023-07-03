6d7bf55a42782b64b2a92b876772b76b3efbb613
Teejay Dixon <tahjgdixon@gmail.com>
2023-07-03 00:47:14 -0700

    maxChar solved in JavaScript
    exit

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+  const chars = {};
+  let max;
+  let maxIndex;
+
+
+  for (let char of str) {
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
+    }
+  }
+  
+
+  max = Math.max(...Object.values(chars));
+  maxIndex = Object.keys(chars).find(keys => chars[keys] === max);
+
+  return maxIndex
+}
+
+console.log(maxChar());
 
 module.exports = maxChar;
